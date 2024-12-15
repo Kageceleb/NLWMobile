3363eaafb1c7f610f46281683cce2d14614559bf
Samuel Sant Anna <samuelsantaanna@gmail.com>
2024-12-15 20:30:00 -0300

    finished button component 26m

--- mobileRN/src/app/index.tsx
+++ mobileRN/src/app/index.tsx
@@ -1,13 +1,18 @@
+import { Button } from "@/components/button"
 import { Steps } from "@/components/steps"
 import { Welcome } from "@/components/welcome"
-import { View, Text } from "react-native"
+import { View } from "react-native"
 
 export default function Index() {
     return (
-        <View style={{flex: 1}}
+        <View style={{ flex: 1 }}
         >
             <Welcome />
             <Steps />
+            <Button >
+                <Button.Title>Começar</Button.Title>
+            </Button>
+
         </View>
     )
 }
